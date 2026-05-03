8f6e55dc1e89a4a6d42bb7244b1b2827812ffc05
arpit kumar chauhan <arpitchauhan2468@gmail.com>
2026-05-03 23:28:25 +0530

    Complete simplification - move app to root, remove monorepo structure

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
-import { createElement } from "react";
 import { createRoot } from "react-dom/client";
-import App from "../artifacts/solana-migrator/src/App";
-import "../artifacts/solana-migrator/src/index.css";
+import App from "./App";
+import "./index.css";
 
-createRoot(document.getElementById("root")!).render(createElement(App));
+createRoot(document.getElementById("root")!).render(<App />);
